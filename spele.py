fb78395e2a8dd034b7de97d3e5892692c1b9c8e1
Marks Tatulcenkovs <A231453MT@rvt.lv>
2024-03-25 16:34:11 +0200

    1. iesaku veidot spēli. Pievienoju kauliņa visas puses un vinu kruzes animaciju

---
+++ spele.py
@@ -0,0 +1,73 @@
+from random import randint
+dice1 = """ 
+╔═════════╗
+║         ║
+║    ■    ║
+║         ║
+╚═════════╝
+"""
+dice2 = """ 
+╔═════════╗
+║      ■  ║
+║         ║
+║  ■      ║
+╚═════════╝
+"""
+dice3 = """ 
+╔═════════╗
+║      ■  ║
+║    ■    ║
+║  ■      ║
+╚═════════╝
+"""
+dice4 = """ 
+╔═════════╗
+║  ■   ■  ║
+║         ║
+║  ■   ■  ║
+╚═════════╝
+"""
+dice5 = """ 
+╔═════════╗
+║  ■   ■  ║
+║    ■    ║
+║  ■   ■  ║
+╚═════════╝
+"""
+dice6 = """ 
+╔═════════╗
+║  ■   ■  ║
+║  ■   ■  ║
+║  ■   ■  ║
+╚═════════╝
+"""
+kruze1 = """
+  ╔══════╗   
+ ║        ║              
+║          ║
+║          ║
+║          ║
+╚══════════╝
+                   
+"""
+
+# Veidošana un kauliņu izvadišana
+
+
+
+"""
+dice = randint(1,6)
+if dice == 1:
+    print(dice1)
+elif dice == 2:
+    print(dice2)
+elif dice == 3:
+    print(dice3)
+elif dice == 4:
+    print(dice4)
+elif dice == 5:
+    print(dice5)
+elif dice == 6:
+    print(dice6)
+"""
+input()
